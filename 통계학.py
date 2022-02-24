3eec2fba85b6aeec397d72123a2deedd3e9dcaf9
Sujin Jang <susan3722@snu.ac.kr>
2022-02-24 22:26:05 +0900

    Solution 2
    - sys를 사용해서 input 받는 시간을 줄임
    - s 변수를 만들어서 sum() 메소드 호출을 줄임
    - count() 메소드 대신 딕셔너리를 이용해서 리스트의 요소를 셈
    - 시간 효율성 통과

--- 통계학.py
+++ 통계학.py
@@ -1,20 +1,25 @@
-N = int(input())
+import sys
+N = int(sys.stdin.readline())
 nums = []
 for i in range(N) :
-    nums.append(int(input()))
+    nums.append(int(sys.stdin.readline()))
 
 nums.sort()
+s = sum(nums)
 
 cnt = {}
 for n in nums :
-    cnt[n] = nums.count(n)
+    if n in cnt :
+        cnt[n] += 1
+    else :
+        cnt[n] = 1
 
 max_cnt = max(cnt.values())
 mcnt = [key for key, value in cnt.items() if value == max_cnt]
 mcnt.sort()
 
-print(int(round( sum(nums) / N, 0 )))
-print(nums[len(nums)//2])
+print(int(round( s / N, 0 )))
+print(nums[N//2])
 if len(mcnt) == 1 :
     print(mcnt[0])
 else :
