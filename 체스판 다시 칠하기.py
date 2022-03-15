86343dbd84df556f61cff9381f908ab95a6445c3
Sujin Jang <susan3722@snu.ac.kr>
2022-03-15 12:50:25 +0900

    Solution
    - 체스판 리스트를 받을때 W, B가 중첩된 리스트 안에서 comma로 분리되어 있어야 좌표를 사용할 수 있음
        - input().split()이 아닌 list(input())으로 받음
    - 8 X 8 만큼의 리스트를 반환해주는 pop_chess 함수 구현
    - 체스판에서 2가지 경우의 수를 비교해서 페인트칠 해야하는 체스판의 개수 중 작은 것을 반환해주는 compare_chess 함수 구현
    - 입력받은 체스판 리스트에서 왼쪽 위를 기준으로 8 X 8을 뽑을 수 있는 좌표들로 for문을 돌림
    - 완전탐색 + 구현 문제

---
+++ 체스판 다시 칠하기.py
@@ -0,0 +1,46 @@
+N, M = map(int, input().split())
+
+chess = []
+for i in range(N) :
+    chess.append(list(input()))
+
+def pop_chess(x, y, lst) :
+    new = []
+    for i in range(x, x + 8) :
+        new.append(list(lst[i][y:y+8]))
+    return new 
+
+def compare_chess(new) :
+    bw = [list('BWBWBWBW'), list('WBWBWBWB')]
+    # case 1
+    cnt1 = 0
+    for i in range(8) :
+        if i % 2 == 0 :
+            for j in range(8) :
+                if new[i][j] != bw[0][j] :
+                    cnt1 += 1
+        else :
+            for j in range(8) :
+                if new[i][j] != bw[1][j] :
+                    cnt1 += 1
+    # case 2
+    cnt2 = 0
+    for i in range(8) :
+        if i % 2 == 0 :
+            for j in range(8) :
+                if new[i][j] != bw[1][j] :
+                    cnt2 += 1
+        else :
+            for j in range(8) :
+                if new[i][j] != bw[0][j] :
+                    cnt2 += 1
+    return min(cnt1, cnt2)
+                
+ans = []
+for i in range(N - 7) :
+    for j in range(M - 7) :
+        new = pop_chess(i, j, chess)
+        ans.append(compare_chess(new))
+
+print(min(ans))
+        
