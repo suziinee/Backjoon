5002d237864301e68e93cfd654b14adcb3d98220
Sujin Jang <susan3722@snu.ac.kr>
2022-04-05 10:17:15 +0900

    Solution 2
    - 3칸 연속 오르는 조건을 추가하는 대신, 점화식을 바꿈
        - 한칸 전에서 올라오는 경우는 무조건 그 전에 두칸 전에서 올라왔을 것이므로 그것을 이용해서 점화식 세우기
        - 한 칸 전의 값 + 세 칸 전의 optimal value + 현재 값 vs 두 칸 전의 optimal value + 현재 값
    - 이때 n == 1일 경우는 3 이상의 for문을 돌리지 못하므로 따로 조건처리를 해두기
    - 1번째 칸부터 계산을 하기 위해 dp, k table 모두 (n + 1)개로 설정

--- 계단오르기.py
+++ 계단오르기.py
@@ -1,22 +1,17 @@
-# a(i) = max(a(i-1) + k(i), a(i-2) + k(i))
+# a(i) = max(k(i-1) + a(i-3) + k[i], a(i-2) + k[i])
 
 n = int(input())
-k = []
+k = [0]
 for i in range(n) :
     k.append(int(input()))
 
-dp = [0] * n 
-dp[0] = k[0]
-dp[1] = k[0] + k[1]
-cnt = 1
+dp = [0] * (n + 1) 
+dp[1] = k[1]
+dp[2] = k[1] + k[2]
 
-for i in range(2, n) :
-    if cnt == 1 :
-        dp[i] = dp[i-2] + k[i]
-        cnt = 0
-    else :
-        dp[i] = max(dp[i-1] + k[i], dp[i-2] + k[i])
-        if dp[i] == dp[i-1] + k[i] :
-            cnt += 1
-
-print(dp[n-1])
+if n == 1 :
+    print(d[1])
+else :
+    for i in range(3, n + 1) :
+        dp[i] = max(k[i-1] + a[i-3] + k[i], a[i-2] + k[i])
+    print(dp[n])
