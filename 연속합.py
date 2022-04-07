19c344c5b23312b35f439914193fd49fc2958580
Sujin Jang <susan3722@snu.ac.kr>
2022-04-07 16:09:57 +0900

    Modified

--- 연속합.py
+++ 연속합.py
@@ -1,9 +1,9 @@
+# dp[i] : i번째까지 연속된 수의 최대 합
+# dp[i] = max(nums[i] + dp[i-1], nums[i])
+
 n = int(input())
 nums = list(map(int, input().split()))
 dp = [0] * n
-
-# dp[i] : i번째까지 연속된 수의 최대 합
-# dp[i] = max(nums[i] + dp[i-1], nums[i])
 
 # 초기화
 dp[0] = nums[0]
