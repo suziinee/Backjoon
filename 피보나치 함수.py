40a0df8bc8dc3f1736153b9453f5a25acb6bcdb7
Sujin Jang <susan3722@snu.ac.kr>
2022-04-08 15:50:45 +0900

    Modified
    - 딕셔너리로 수정

--- 피보나치 함수.py
+++ 피보나치 함수.py
@@ -5,15 +5,14 @@ T = int(input())
 
 for t in range(T) :
 
-    zero = [1, 0, 1]
-    one = [0, 1, 1]
+    zero = {0 : 1, 1: 0, 2 : 1}
+    one = {0 : 0, 1 : 1, 2 : 1}
 
     def fibo(n) :
-        length = len(zero)
-        if n >= length :
-            for i in range(length, n + 1) :    
-                zero.append(zero[i-1] + zero[i-2])
-                one.append(one[i-1] + one[i-2])
+        if n not in zero :
+            for i in range(3, n + 1) :    
+                zero[i] = zero[i-1] + zero[i-2]
+                one[i] = one[i-1] + one[i-2]
         print(zero[n], one[n])
     
     fibo(int(input()))
