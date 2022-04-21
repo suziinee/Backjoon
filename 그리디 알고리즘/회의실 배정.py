afc2b9253b839d5316e92dbb0d3320c10aaeba2c
Sujin Jang <susan3722@snu.ac.kr>
2022-04-21 16:18:13 +0900

    Solution 2
    - greedy : 회의실을 끝나는 시간 순서대로 정렬하고 끝나는 시간이 같으면 시작하는 순서대로 정렬
        - 이후 맨 첫번째 값을 기준으로 꼬리물기해서 세어주면 됨
        - 이렇게 정렬하면 무조건 최댓값이 나옴

--- 그리디 알고리즘/회의실 배정.py
+++ 그리디 알고리즘/회의실 배정.py
@@ -1,26 +1,18 @@
+import sys
 N = int(input())
 times = []
-ans = []
-from bisect import bisect_left
 
 for i in range(N) :
-    times.append(list(map(int, input().split())))
+    times.append(list(map(int, sys.stdin.readline().split())))
 
-# 시작시간으로 정렬
-times = sorted(times, key = lambda x : x[0])
-start_time = [x[0] for x in times]
-end_time = [x[1] for x in times]
+# 끝나는 시간으로 정렬하되 끝나는 시간이 같으면 시작 시간으로 정렬
+times = sorted(times, key = lambda x : (x[1], x[0]))
 
-# 가장 첫 시작시간의 끝시간보다 시작시간이 큰 구간은 탐색하지 않아도 됨
-border = bisect_left(start_time, end_time[0])
+end = times[0][1]
+cnt = 1
+for i in range(1, N) :
+    if times[i][0] >= end :
+        cnt += 1
+        end = times[i][1]
 
-for i in range(border) :
-    end = end_time[i]
-    res = 1
-    while end <= start_time[-1] : #더이상 시작할 수 없을때까지 반복
-        res += 1
-        next_ = bisect_left(start_time, end) #다음 시작시간 index
-        end = end_time[next_]
-    ans.append(res)
-
-print(max(ans))
+print(cnt)
