9526184cb0466f6b909d6ef513c352559dfc31c1
Sujin Jang <susan3722@snu.ac.kr>
2022-04-26 13:04:03 +0900

    Modified
    - 딕셔너리 방식으로 했는데도 메모리 초과
    -> 저장할때도 %15746을 해줬어야 함

--- 동적 계획법/01타일.py
+++ 동적 계획법/01타일.py
@@ -2,11 +2,12 @@
 
 N = int(input())
 
-dp = [0] * (N + 1)
-dp[1] = 1
-dp[2] = 2
+dp = {1 : 1, 2 : 2}
 
 for i in range(3, N + 1) :
-    dp[i] = dp[i-1] + dp[i-2]
+    if i in dp :
+        continue
+    else :
+        dp[i] = (dp[i-2] + dp[i-1]) % 15746
 
-print(dp[N] % 15746)
+print(dp[N])
