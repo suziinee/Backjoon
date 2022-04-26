899841349a13eb48fd987ea1662fbbd5b7c44846
Sujin Jang <susan3722@snu.ac.kr>
2022-04-26 12:14:31 +0900

    Modified

--- 동적 계획법/계단오르기.py
+++ 동적 계획법/계단오르기.py
@@ -5,13 +5,14 @@ k = [0]
 for i in range(n) :
     k.append(int(input()))
 
-dp = [0] * (n + 1) 
-dp[1] = k[1]
-dp[2] = k[1] + k[2]
-
 if n == 1 :
-    print(d[1])
-else :
+    print(k[1])
+else :  
+    dp = [0] * (n + 1)
+    dp[1] = k[1]
+    dp[2] = k[1] + k[2]
+
     for i in range(3, n + 1) :
-        dp[i] = max(k[i-1] + a[i-3] + k[i], a[i-2] + k[i])
+        dp[i] = max(k[i-1] + dp[i-3] + k[i], dp[i-2] + k[i])
+
     print(dp[n])
