0b7be4722f7e3882dd1a9a506edf2a358649db01
DESKTOP-TPDLR5H\user <jsj032097@gmail.com>
2022-08-01 14:21:44 +0900

    Solution 2 - 조합을 dfs로 구현 - 하나의 조합이 완성될때마다 calcul 함수를 호출해서 바로바로 차이 계산 - 차이 계산 후 dfs 함수에서 min_ 갱신 - itertools 사용 때보다 메모리는 적게 사용됐지만 시간은 더 오래걸림

--- 삼성 코테 기출/14889 스타트와 링크.py
+++ 삼성 코테 기출/14889 스타트와 링크.py
@@ -1,27 +1,35 @@
-from itertools import combinations
-from collections import deque
-
 N = int(input())
 arr = [[] for _ in range(N)]
 for i in range(N) :
   arr[i].extend(list(map(int, input().split())))
 
-combs = deque(combinations(list(range(N)), N//2))
+nums = [i for i in range(N)]
+combs = [[0] * (N//2), []]
 min_ = 100000
 
-while combs :
-  start = combs.popleft()
+def calcul(start, link) :
   start_score = 0
-  link = combs.pop()
   link_score = 0
-
   for s in start :
-    start_score += sum(arr[s]) - sum([arr[s][i] for i in link])
-
+    start_score += sum(arr[s]) - sum([arr[s][l] for l in link])
   for l in link :
-    link_score += sum(arr[l]) - sum([arr[l][i] for i in start])
-    
-  if min_>abs(start_score - link_score) :
-    min_ = abs(start_score - link_score)
+    link_score += sum(arr[l]) - sum([arr[l][s] for s in start])
+  return abs(start_score - link_score)
 
+def dfs(L, BeginWith) :
+  global min_
+  if L == N//2 :
+    for n in nums :
+      if n not in combs[0] :
+        combs[1].append(n)
+    res = calcul(combs[0], combs[1])
+    if res < min_ :
+      min_ = res
+    combs[1] = []    
+  else :
+    for i in range(BeginWith, N) :
+      combs[0][L] = nums[i]
+      dfs(L+1, i+1)
+
+dfs(0, 0)
 print(min_)
