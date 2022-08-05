d9935e7c4e1a31cb12a58b6e45839cf3744bd6e2
suziinee <susan3722@snu.ac.kr>
2022-08-05 16:22:05 +0900

    Solution 3 - 유투브에서 보았던 map 2개를 이용하는 방식     - dirt 시에 cur에서 next로 미세먼지 이동 후 cur 리스트는 0으로 초기화하고 cur는 next가 됨 - C에서는 통과되자만 파이썬은 3중리스트는 사용하면 안될듯..!!

--- 삼성 코테 기출/17144 미세먼지 안녕!.py
+++ 삼성 코테 기출/17144 미세먼지 안녕!.py
@@ -1,79 +1,83 @@
-def dirt(arr) :
+r, c, t = map(int, input().split())
+arr = [ [list(map(int, input().split())) for _ in range(r)], [[0]*c for _ in range(r)] ]
+cur = 0
+
+#미세먼지 좌표 찾기
+flag = 0
+for i in range(r) :
+    for j in range(c) :
+        if arr[cur][i][j] == -1 and flag == 0 :
+            up_x = i
+            flag = 1
+        if arr[cur][i][j] == -1 and flag == 1 :
+            down_x = i
+            break
+
+arr[1][up_x][0] = -1
+arr[1][down_x][0] = -1
+
+
+def circulation() :
+    global cur
+
+    for i in range(up_x-2, -1, -1) :
+        arr[cur][i+1][0] = arr[cur][i][0]
+    for i in range(1, c) :
+        arr[cur][0][i-1] = arr[cur][0][i]
+    for i in range(1, up_x+1) :
+        arr[cur][i-1][c-1] = arr[cur][i][c-1]
+    for i in range(c-2, 0, -1) :
+        arr[cur][up_x][i+1] = arr[cur][up_x][i]
+    arr[cur][up_x][1] = 0
+
+    for j in range(down_x+2, r) :
+        arr[cur][j-1][0] = arr[cur][j][0]
+    for j in range(1, c) :
+        arr[cur][r-1][j-1] = arr[cur][r-1][j]
+    for j in range(r-2, down_x-1, -1) :
+        arr[cur][j+1][c-1] = arr[cur][j][c-1]
+    for j in range(c-2, 0, -1) :
+        arr[cur][down_x][j+1] = arr[cur][down_x][j]
+    arr[cur][down_x][1] = 0
+
+
+def dirt() :
+    global cur
+    global up_x
+    global down_x
+    next = (cur+1)%2
     dx = [-1, 0, 1, 0]
     dy = [0, 1, 0, -1]
-    for i in range(1, r+1) :
-        for j in range(1, c+1) :
-            if (arr[i][j][0] != 0) and (arr[i][j][0] != -1) :
-                cnt = 0
-                for n in range(4) :
-                    nx = i + dx[n]
-                    ny = j + dy[n]
-                    if nx<1 or ny<1 or nx>r or ny>c : continue
-                    if arr[nx][ny][0] == -1 : continue
-                    arr[nx][ny].append(arr[i][j][0]//5) #더할것
-                    cnt += 1
-                arr[i][j][0] -= ((arr[i][j][0]//5) * cnt)
-    for i in range(1, r+1) :
-        for j in range(1, c+1) :
-            arr[i][j] = [sum(arr[i][j])]
 
-def circulation(arr, x, y) :
-    new_arr = [[0] * (c+1) for _ in range(r+1)]
-    new_arr = [[[new_arr[i][j]] for j in range(c + 1)] for i in range(r + 1)]
-    # (x, y)에서 시작되는 위쪽 반시게 순환
-    for i in range(1, x+1) :
-        for j in range(1, c+1) :
-            if i==x and j==y : continue
-            if arr[i][j][0] != 0 :
-                if i==1 and j==1 : new_arr[2][1][0] = arr[i][j][0]
-                elif i==x and j==1 : new_arr[x][2][0] = arr[i][j][0]
-                elif i==x and j==c : new_arr[x-1][c][0] = arr[i][j][0]
-                elif i==1 and j==c : new_arr[1][c-1][0] = arr[i][j][0]
-                elif i==x : new_arr[i][j+1][0] = arr[i][j][0]
-                elif j==c : new_arr[i-1][j][0] = arr[i][j][0]
-                elif i==1 : new_arr[i][j-1][0] = arr[i][j][0]
-                elif j==1 : new_arr[i+1][j][0] = arr[i][j][0]
-                else :
-                    new_arr[i][j][0] = arr[i][j][0]
-            new_arr[x][y][0] = -1
-    #(x+1, y)에서 시작하는 아래 시계 순환
-    for i in range(x+1, r+1) :
-        for j in range(1, c+1) :
-            if i==(x+1) and j==y : continue
-            if arr[i][j][0] != 0 :
-                if i==x+1 and j==1 : new_arr[x+1][2][0] = arr[i][j][0]
-                elif i==r and j==1 : new_arr[r-1][1][0] = arr[i][j][0]
-                elif i==r and j==c : new_arr[r][c-1][0] = arr[i][j][0]
-                elif i==x+1 and j==c : new_arr[x+2][c][0] = arr[i][j][0]
-                elif i==x+1 : new_arr[i][j+1][0] = arr[i][j][0]
-                elif j==c : new_arr[i+1][j][0] = arr[i][j][0]
-                elif i==r : new_arr[i][j-1][0] = arr[i][j][0]
-                elif j==1 : new_arr[i-1][j][0] = arr[i][j][0]
-                else :
-                    new_arr[i][j][0] = arr[i][j][0]
-            new_arr[x+1][y][0] = -1
-    return new_arr
+    for i in range(r) :
+        for j in range(c) :
+            if arr[cur][i][j] != 0 and arr[cur][i][j] != -1 :
+                discount = 0
+                val = arr[cur][i][j]//5
+                for d in range(4) :
+                    nx = i + dx[d]
+                    ny = j + dy[d]
+                    if nx<0 or ny<0 or nx>=r or ny>=c : continue
+                    if arr[cur][nx][ny] == -1 : continue
+                    arr[next][nx][ny] += val
+                    discount += val
+                arr[next][i][j] += (arr[cur][i][j] - discount)
 
-def find_circulation() :
-    for i in range(1, r+1) :
-        for j in range(1, c+1) :
-            if arr[i][j][0] == -1 : return (i, j)
+    # cur 초기화
+    for i in range(r) :
+        for j in range(c) :
+            if arr[cur][i][j] != -1 :
+                arr[cur][i][j] = 0
 
-# 입력 받기
-r, c, t = map(int, input().split())
-arr = [[0] * (c+1) for _ in range(r+1)]
-for i in range(1, r+1) :
-    arr[i][1:] = list(map(int, input().split()))
-arr = [[[arr[i][j]] for j in range(c+1)] for i in range(r+1)]
-x, y = find_circulation()
+    cur = next
 
-# t초가 지난 후 남아있는 미세먼지 양
+
 for time in range(t) :
-    dirt(arr)
-    arr = circulation(arr, x, y)
+    dirt()
+    circulation()
 
 ans = 0
-for i in range(1, r+1) :
-    for j in range(1, c+1) :
-        ans += arr[i][j][0]
-print(ans + 2)
+for i in range(r) :
+    for j in range(c) :
+        ans += arr[cur][i][j]
+print(ans+2)
