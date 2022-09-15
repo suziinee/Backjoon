a2d8d52e0abf92fefa13d72a36049682f56b14bf
suziinee <susan3722@snu.ac.kr>
2022-09-15 17:23:05 +0900

    Solution 2 - conn_min을 이용해서 같은 경로에서 최소 거리만 conn 배열에 넣고 싶었는데 메모리 초과로 실패

--- BFS/1753 최단경로.cpp
+++ BFS/1753 최단경로.cpp
@@ -8,13 +8,14 @@ int V, E, K;
 
 struct CONN { int n; int val; };
 list<CONN> conn[MAXV + 1];
+int conn_min[MAXV + 1][MAXV + 1];
 
 struct STATUS { int cur; int sum; };
 struct COMP {
 	bool operator()(STATUS& s1, STATUS& s2) { return s1.sum > s2.sum; }
 };
 priority_queue<STATUS, vector<STATUS>, COMP> pq;
-int chk[MAXV + 1]; 
+int chk[MAXV + 1];
 
 
 void input()
@@ -22,9 +23,13 @@ void input()
 	cin >> V >> E >> K;
 
 	int s, n, val;
+	fill(&conn_min[0][0], &conn_min[MAXV][MAXV + 1], 0x7fffffff);
+
 	for (int i = 0; i < E; i++) {
 		cin >> s >> n >> val;
+		if (conn_min[s][n] <= val) continue;
 		conn[s].push_back({ n, val });
+		conn_min[s][n] = val;
 	}
 }
 
