565fda7bfdcb00913abf0b274dcb65e09b1ccd6f
suziinee <susan3722@snu.ac.kr>
2022-09-15 13:43:21 +0900

    Solution 2 - 점수 구하는 방법을 더 효율적으로 해서 시간을 줄임

--- DFS/15661 링크와 스타트.cpp
+++ DFS/15661 링크와 스타트.cpp
@@ -1,66 +1,60 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 #define MAXN 20
 int N;
 int map[MAXN][MAXN];
-int pick[MAXN];
+int team1[MAXN]; int team2[MAXN];
+int pick[MAXN]; //1과 0으로 구분
 int ans = 0x7fffffff;
-struct STATUS { int start; int link; };
 
 
-void input()
+void input() 
 {
 	cin >> N;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cin >> map[i][j];
+	for (int y = 0; y < N; y++) {
+		for (int x = 0; x < N; x++) {
+			cin >> map[y][x];
 		}
 	}
 }
 
-STATUS check(int n)
+int check()
 {
-	int start = 0;
-	for (int i = 0; i <= n; i++) {
-		for (int j = 0; j <= n; j++) {
-			start += map[pick[i]][pick[j]];
-		}
-	}
-	
-	vector<int> nonpick;
+	int t1 = 0; int t2 = 0;
 	for (int i = 0; i < N; i++) {
-		int flag = 0;
-		for (int j = 0; j <= n; j++) {
-			if (i == pick[j]) { flag = 1; break; }
-		}
-		if (!flag) nonpick.push_back(i);
+		if (pick[i]) team1[t1++] = i;
+		else team2[t2++] = i;
 	}
 
-	int link = 0;
-	for (int i = 0; i < nonpick.size(); i++) {
-		for (int j = 0; j < nonpick.size(); j++) {
-			link += map[nonpick[i]][nonpick[j]];
+	int sum1 = 0; int sum2 = 0;
+	for (int i = 0; i < t1; i++) {
+		for (int j = i + 1; j < t1; j++) {
+			sum1 += map[team1[i]][team1[j]] + map[team1[j]][team1[i]];
+		}
+	}
+	for (int i = 0; i < t2; i++) {
+		for (int j = i + 1; j < t2; j++) {
+			sum2 += map[team2[i]][team2[j]] + map[team2[j]][team2[i]];
 		}
 	}
 
-	return { start, link };
+	return abs(sum1 - sum2);
 }
 
-void dfs(int n, int s, int diff)
+void dfs(int n, int s, int diff) 
 {
-	if (ans > diff) ans = diff;
+	if (diff < ans) ans = diff;
 	if (n == N - 1) return;
 
-	for (int i = s; i < N; i++) {
-		pick[n] = i;
-		STATUS s = check(n);
-		dfs(n + 1, i + 1, abs(s.start - s.link));
+	for (int i = s; i < N; i++) { //i번째를 뽑아서 start에 넣는 것
+		pick[i] = 1;
+		dfs(n + 1, i + 1, check());
+		pick[i] = 0;
 	}
 }
 
-void solve()
+void solve() 
 {
 	dfs(0, 0, 0x7fffffff);
 	cout << ans;
