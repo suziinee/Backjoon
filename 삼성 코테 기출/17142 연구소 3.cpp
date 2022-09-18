c8c10cedd47f186208fc5907d3d6eff9f00a859b
suziinee <susan3722@snu.ac.kr>
2022-09-19 00:39:44 +0900

    Solution 1 - 2인 좌표 중에서 M개를 조합으로 선정 -> dfs - 비활성 바이러스 처리 : 일단 시간은 증가시키고 나중에 max_time을 갱신할때 포함시키지 않음     - 처음에 시간을 증가시키지 않고 큐에 넣었는데 그러면 평등하게 증가하지 않는 문제가 생김 - 시간을 저장하는 배열을 chk로 따로 만들어서 구현

---
+++ 삼성 코테 기출/17142 연구소 3.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+#define MAXN 50
+#define MAXM 10
+int N, M;
+int map[MAXN][MAXN];
+int ans = 0x7fffffff;
+
+struct AXIS { int y; int x; };
+vector<AXIS> virus; //2의 좌표 모음
+AXIS pick[MAXM];
+
+queue<AXIS> q;
+int time[MAXN][MAXN];
+
+
+void input()
+{
+	cin >> N >> M;
+	for (int y = 0; y < N; y++) {
+		for (int x = 0; x < N; x++) {
+			cin >> map[y][x];
+			if (map[y][x] == 2) virus.push_back({ y, x });
+		}
+	}
+}
+
+int count_time()
+{
+	static int dx[] = { 0, 1, 0, -1 };
+	static int dy[] = { -1, 0, 1, 0 };
+
+	q = {};
+	fill(&time[0][0], &time[MAXN - 1][MAXN], -1);
+	for (int i = 0; i < M; i++) {
+		q.push(pick[i]);
+		time[pick[i].y][pick[i].x] = 0; 
+	}
+
+	while (!q.empty()) {
+		AXIS cur = q.front(); q.pop();
+
+		for (int d = 0; d < 4; d++) {
+			int nx = cur.x + dx[d];
+			int ny = cur.y + dy[d];
+
+			if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+			
+			if (map[ny][nx] != 1 && time[ny][nx] == -1) {
+				time[ny][nx] = time[cur.y][cur.x] + 1;
+				q.push({ ny, nx });
+			}
+		}
+	}
+
+	//전부 감염되었는지 확인 및 최대 시간 구하기
+	int max_time = 0;
+	bool complete = true;
+	for (int y = 0; y < N; y++) {
+		for (int x = 0; x < N; x++) {
+			if (map[y][x] == 0 && time[y][x] == -1) { complete = false; break; }
+			if (map[y][x] == 0) max_time = max(max_time, time[y][x]);
+		}
+	}
+
+	if (complete) return max_time;
+	else return 0x7fffffff;
+}
+
+void dfs_virus(int s, int n)
+{
+	if (n == M) {
+		int t = count_time();
+		if (ans > t) ans = t;
+		return;
+	}
+
+	for (int i = s; i < virus.size(); i++) {
+		pick[n] = virus[i];
+		dfs_virus(i + 1, n + 1);
+	}
+}
+
+void solve()
+{
+	dfs_virus(0, 0);
+
+	if (ans == 0x7fffffff) cout << -1;
+	else cout << ans;
+}
+
+
+int main(void)
+{
+	input();
+	solve();
+	return 0;
+}
