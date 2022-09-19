cfefe4a460a5201251bcdf7467233ba1c3332e65
suziinee <susan3722@snu.ac.kr>
2022-09-19 21:21:12 +0900

    Solution 6 - 강사님 방법 - next.k를 벽이 있느냐 없느냐에 따라서 cur.k와 cur.k+1로 삼항식 - next.k를 설정 전에 벽이고 cur.k가 K 이상이면 continue 해주는 거 잊지 말 것 - 가장 시간이 빨랐음

--- BFS/14442 벽 부수고 이동하기 2.cpp
+++ BFS/14442 벽 부수고 이동하기 2.cpp
@@ -28,6 +28,8 @@ int bfs()
 	static int dx[] = { 0, 1, 0, -1 };
 	static int dy[] = { -1, 0, 1, 0 };
 
+	vector<int> candi;
+
 	q.push({ 0, 0, 0, 1 });
 	chk[0][0][0] = true;
 	if (N - 1 == 0 && M - 1 == 0) return 1;
@@ -36,33 +38,22 @@ int bfs()
 		AXIS cur = q.front(); q.pop();
 
 		for (int d = 0; d < 4; d++) {
-			int nx = cur.x + dx[d];
-			int ny = cur.y + dy[d];
+			AXIS next;
+			next.x = cur.x + dx[d];
+			next.y = cur.y + dy[d];
+			next.dist = cur.dist + 1;
 
-			if (nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
+			if (next.x < 0 || next.y < 0 || next.x >= M || next.y >= N) continue;
+			if (map[next.y][next.x] == '1' && cur.k == K) continue;
+			next.k = (map[next.y][next.x] == '1') ? (cur.k + 1) : cur.k;
 
-			if (map[ny][nx] == '0') { //0일때 그대로 이동
-				if (chk[ny][nx][cur.k] == false) {
-					chk[ny][nx][cur.k] = true;
-					q.push({ ny, nx, cur.k, cur.dist + 1 });
-					if (ny == N - 1 && nx == M - 1) return cur.dist + 1;
-				}
-			}
-			else { //벽일때
-				if (cur.k >= K) { //더이상 벽을 부술 수 없음
-					continue;
-				}
-				else if (cur.k < K) { //벽을 부술 수 있음
-					if (chk[ny][nx][cur.k + 1] == false) {
-						chk[ny][nx][cur.k + 1] = true;
-						q.push({ ny, nx, cur.k + 1, cur.dist + 1 });
-						if (ny == N - 1 && nx == M - 1) cur.dist + 1;
-					}
-				}
+			if (chk[next.y][next.x][next.k] == false) {
+				chk[next.y][next.x][next.k] = true;
+				q.push(next);
+				if (next.y == N - 1 && next.x == M - 1) return next.dist;
 			}
 		}
 	}
-
 	return -1;
 }
 
