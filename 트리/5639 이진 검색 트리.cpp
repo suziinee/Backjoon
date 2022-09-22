4f6fc4e21f7f87d6d062cef5240fb5f23c911e3f
DESKTOP-L4QE09O\jsj03 <susan3722@snu.ac.kr>
2022-09-22 17:34:57 +0900

    Solution 3 - search를 하면서 왼쪽-오른쪽-루트 순서의 후위 순회도 함께 진행

--- 트리/5639 이진 검색 트리.cpp
+++ 트리/5639 이진 검색 트리.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 using namespace std;
 
-struct NODE { int l; int r; };
-unordered_map<int, NODE> bst;
-int root = -1;
 vector<int> vec;
 
 
@@ -20,43 +16,27 @@ void input()
 	}
 }
 
-void post(int n)
-{
-	if (n == -1) return;
-	post(bst[n].l);
-	post(bst[n].r);
-	cout << n << "\n";
-}
-
-void search(int p, int s, int e)
+void dfs(int s, int e)
 {
 	if (s > e) return;
+	if (s == e) {
+		cout << vec[s] << "\n";
+		return;
+	}
 
 	int m;
 	for (m = s; m <= e; m++) {
-		if (p < vec[m]) break;
+		if (vec[s] < vec[m]) break; //나보다 큰 걸 찾으면 break (오른쪽 tree의 시작점 찾음)
 	}
 
-	if (s <= m - 1) {
-		bst[p].l = vec[s];
-		bst.insert({ vec[s], {-1, -1} });
-		search(vec[s], s + 1, m - 1);
-	}
-	if (m <= e) {
-		bst[p].r = vec[m];
-		bst.insert({ vec[m], {-1, -1} });
-		search(vec[m], m + 1, e);
-	}
+	dfs(s + 1, m - 1);
+	dfs(m, e);
+	cout << vec[s] << "\n";
 }
 
 void solve()
 {
-	root = vec[0];
-	bst.insert({ root, {-1, -1} });
-	
-	search(root, 1, vec.size() - 1);
-	
-	post(root);
+	dfs(0, vec.size() - 1);
 }
 
 
