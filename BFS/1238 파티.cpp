0f45297f1231e1363fee556428051c169f959681
DESKTOP-L4QE09O\jsj03 <susan3722@snu.ac.kr>
2022-09-26 15:50:54 +0900

    Solution 2 - i->N까지의 거리와 N->i까지의 거리로 bfs를 2번 돌림 - i->N은 conn_rev, chk를 넘겨줘서 1~N에서 X로 가는 길을 X부터 거꾸로 찾게 함     -> conn_rev를 넣어주는 이유는 X에서 거꾸로 찾아야 하기 때문!!**** - N->i는 conn 배열을 넣어주고 N부터 넣어서 bfs를 돌림     -> 이때 경로는 정상이고 방향만 N에서 i로 rev이기 때문에 경로 배열은 conn으로 넣어줘야함

--- BFS/1238 파티.cpp
+++ BFS/1238 파티.cpp
@@ -8,9 +8,11 @@ int N, M, X;
 
 struct CONN { int n; int cost; };
 vector<CONN> conn[MAXN + 1];
+vector<CONN> conn_rev[MAXN + 1];
 
-int chk[MAXN + 1][MAXN + 1]; //시작지점, 도착지점까지 최단거리
-struct STATUS { int start; int cur; int sum_cost; };
+int chk[MAXN + 1]; //1~N에서 X까지 최단거리
+int chk_rev[MAXN + 1]; //X에서 1~N까지 최단거리
+struct STATUS { int cur; int sum_cost; };
 queue<STATUS> q;
 
 int dist[MAXN + 1]; //1~N의 오고 가는 최단거리
@@ -24,29 +26,27 @@ void input()
 	for (int i = 0; i < M; i++) {
 		cin >> u >> v >> cost;
 		conn[u].push_back({ v, cost });
+		conn_rev[v].push_back({ u, cost });
 	}
 }
 
-void bfs() //1~N에서 1~N까지의 거리 구하기
+void bfs(vector<CONN>* adj, int* chk) //X에서 1~N까지 최단거리 
 {
 	q = {};
-	fill(&chk[0][0], &chk[MAXN][MAXN + 1], 0x7fffffff);
+	fill(chk, chk + MAXN + 1, 0x7fffffff);
 
-	for (int i = 1; i <= N; i++) {
-		q.push({ i, i, 0 }); //start, cur, sum_cost
-		chk[i][i] = 0; //start, cur
-	}
+	q.push({ X, 0 });
+	chk[X] = 0;
 
 	while (!q.empty()) {
 		STATUS data = q.front(); q.pop();
 
-		for (CONN c : conn[data.cur]) {
+		for (CONN c : adj[data.cur]) {
 			STATUS ndata;
-			ndata.start = data.start;
 			ndata.cur = c.n;
 			ndata.sum_cost = data.sum_cost + c.cost;
-			if (chk[ndata.start][ndata.cur] <= ndata.sum_cost) continue;
-			chk[ndata.start][ndata.cur] = ndata.sum_cost;
+			if (chk[ndata.cur] <= ndata.sum_cost) continue;
+			chk[ndata.cur] = ndata.sum_cost;
 			q.push(ndata);
 		}
 	}
@@ -54,14 +54,12 @@ void bfs() //1~N에서 1~N까지의 거리 구하기
 
 void solve()
 {
-	bfs();
-	for (int i = 1; i <= N; i++) {
-		dist[i] += chk[i][X]; //1~N에서 X까지
-		dist[i] += chk[X][i]; //X에서 1~N까지
-	}
+	bfs(conn_rev, chk);
+	bfs(conn, chk_rev);
 
 	int max = -1;
 	for (int i = 1; i <= N; i++) {
+		dist[i] += chk[i] + chk_rev[i];
 		if (dist[i] > max) max = dist[i];
 	}
 	cout << max;
