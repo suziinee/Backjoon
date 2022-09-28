eaa21f5eaf5be5c56da28987a0c7db4236df3c25
DESKTOP-L4QE09O\jsj03 <susan3722@snu.ac.kr>
2022-09-29 02:06:40 +0900

    Solution 2 - STATUS 구조체를 만들어서 내구도와 로봇 여부를 관리 - 여전히 st, en 포인터를 사용

--- 삼성 코테 기출/20055 컨베이어 벨트 위의 로봇.cpp
+++ 삼성 코테 기출/20055 컨베이어 벨트 위의 로봇.cpp
@@ -1,85 +1,83 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
 #include <vector>
-#include <deque>
 using namespace std;
 
-int n, k;
-vector<int> belt;
+#define MAXN 100
+int N, K;
+
+struct STATUS {
+	int dur; //내구도
+	int robot; //로봇 개수
+};
+STATUS belt[2 * MAXN + 1]; //1번 벨트부터 2N번 벨트까지 사용
+int st, en;
 
 
 void input()
 {
-	cin >> n >> k;
-	for (int i = 0; i < 2 * n; i++) {
-		int val;
-		cin >> val;
-		belt.push_back(val);
+	cin >> N >> K;
+	for (int i = 1; i <= 2 * N; i++) {
+		cin >> belt[i].dur;
 	}
+	st = 1; en = N;
 }
 
-void output(const int& ans)
+void solve()
 {
-	cout << ans;
-}
-
-int solve()
-{
+	bool is_update = true;
 	int ans = 0;
 
-	int robot[200 + 10]; //로봇이 있는 벨트의 인덱스는 1
-	memset(robot, 0, sizeof(robot));
-	int st = 0; int en = n - 1; //처음에는 0번부터 n-1번까지 컨베이어 벨트
-
-	while (true) {
+	while (is_update) {
+		is_update = false;
 		ans++;
 
-		//1. 벨트 한 칸 회전
-		st = (st == 0) ? 2 * n - 1 : st - 1;
-		en = (en == 0) ? 2 * n - 1 : en - 1;
-		//1-1. 내릴 수 있는 로봇이 있다면 내리기
-		if (robot[en] == 1) robot[en] = 0;
+		//회전
+		st = (st == 1) ? 2 * N : (st - 1);
+		en = (en == 1) ? 2 * N : (en - 1);
+		/*로봇 내리기*/
+		if (belt[en].robot) belt[en].robot = 0;
 
-		//2. 로봇 이동 **en부터 st의 순서로 발생
-		int chk = (en == 0) ? 2 * n - 1 : en - 1;
+		//en부터 st까지 벨트 확인
+		int cur = en;
 		while (true) {
-			if (robot[chk] == 1) {
-				int next = (chk + 1) % (2 * n);
-				if (belt[next] >= 1 && robot[next] == 0) {
-					belt[next]--;
-					robot[chk] = 0;
-					robot[next] = 1;
-				}
-				//2-1. 내릴 수 있으면 바로 내리기
-				if (robot[en] == 1) robot[en] = 0;
+			int next = (cur == 2 * N) ? 1 : (cur + 1);
+			if (belt[cur].robot && belt[next].dur >= 1 && belt[next].robot == 0) {
+				//로봇 이동
+				belt[cur].robot = 0;
+				belt[next].robot = 1;
+				belt[next].dur -= 1;
 			}
-			if (chk == st) break;
-			chk = (chk == 0) ? (chk = 2 * n - 1) : (chk -= 1);
+			/*로봇 내리기*/
+			if (belt[en].robot) belt[en].robot = 0;
+
+			if (cur == st) break;
+			cur = (cur == 1) ? 2 * N : (cur - 1);
 		}
-		
-		//3. 로봇 올리기
-		if (belt[st] >= 1) {
-			robot[st] = 1;
-			belt[st]--;
+		/*로봇 내리기*/
+		if (belt[en].robot) belt[en].robot = 0;
+
+		//올리기
+		if (belt[st].dur >= 1) {
+			belt[st].dur -= 1;
+			belt[st].robot = 1;
 		}
 
-		//4. 벨트 체크
+		//종료조건 확인
 		int cnt = 0;
-		for (auto b : belt) {
-			if (b == 0) cnt++;
+		for (int i = 1; i <= 2 * N; i++) {
+			if (belt[i].dur == 0) cnt++;
 		}
-		if (cnt >= k) break;
+		if (cnt >= K) is_update = false;
+		else is_update = true;
 	}
 
-	return ans;
+	cout << ans;
 }
 
 
 int main()
 {
 	input();
-	int ans = solve();
-	output(ans);
+	solve();
 	return 0;
 }
