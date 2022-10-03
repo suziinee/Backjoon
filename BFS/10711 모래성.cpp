ae88d8716abbcf68e98aa770bf47179e79f45429
DESKTOP-L4QE09O\jsj03 <susan3722@snu.ac.kr>
2022-10-03 22:18:05 +0900

    Solution 2 - 우선순위 큐 +  unordered set으로 chk 배열 - 파도에 영향을 받은 지점의 8방향을 큐에 넣고 매번 돌림 - 이때 이미 넣은 곳인지 확인하기 위해 체크 배열로 set을 사용했고 한 회차 wave를 판단하기 위해 wave 오름차순으로 정렬하는 우선순위 큐 사용 - 이것도 정답은 나오지만 시간초과...

--- BFS/10711 모래성.cpp
+++ BFS/10711 모래성.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <queue>
+#include <unordered_set>
 using namespace std;
 
 #define MAX 1000
 int H, W;
 char map[2][MAX][MAX];
 int cur;
+
+struct STATUS {
+	int y; int x; int wave;
+	bool operator<(const STATUS& s) const {
+		return wave > s.wave;
+	}
+	bool operator==(const STATUS& s) const {
+		return (y == s.y) && (x == s.x) && (wave == s.wave);
+	}
+};
+struct HASH {
+	size_t operator()(const STATUS& s) const {
+		return s.y ^ s.x ^ s.wave;
+	}
+};
+priority_queue<STATUS> pq;
+unordered_set<STATUS, HASH> chk;
 
 
 void input()
@@ -13,39 +32,51 @@ void input()
 	for (int y = 0; y < H; y++) {
 		for (int x = 0; x < W; x++) {
 			cin >> map[0][y][x];
+			map[1][y][x] = map[0][y][x];
+			if (map[0][y][x] != '.') {
+				pq.push({ y, x, 1 });
+				chk.insert({ y, x, 1 });
+			}
 		}
 	}
 }
 
-void wave()
+void wave(int n) //cur에서 next로 기록
 {
 	static int dx[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 	static int dy[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
 
 	int next = (cur + 1) % 2;
-	for (int y = 0; y < H; y++) {
-		for (int x = 0; x < W; x++) {
-			if (map[cur][y][x] == '.') {
-				map[next][y][x] = '.';
-				continue;
-			}
+	while (!pq.empty()) {
+		STATUS data = pq.top(); pq.pop();
+		if (data.wave == n + 1) {
+			pq.push(data);
+			return;
+		}
 
-			int cnt = 0;
+		int cnt = 0;
+		for (int d = 0; d < 8; d++) {
+			int nx = data.x + dx[d];
+			int ny = data.y + dy[d];
+			if (map[cur][ny][nx] == '.') cnt++;
+		}
+		if (cnt >= map[cur][data.y][data.x] - '0') {
+			map[next][data.y][data.x] = '.';
+			//8방향 모래성 모두 pq에 넣기
 			for (int d = 0; d < 8; d++) {
-				int nx = x + dx[d];
-				int ny = y + dy[d];
-				if (map[cur][ny][nx] == '.') cnt++;
-			}
-			if (cnt >= map[cur][y][x] - '0') { //무너짐
-				map[next][y][x] = '.';
-			}
-			else {
-				map[next][y][x] = map[cur][y][x];
+				int nx = data.x + dx[d];
+				int ny = data.y + dy[d];
+				auto it = chk.find({ ny, nx, data.wave + 1 });
+				if (it == chk.end() && map[cur][ny][nx] != '.') {
+					pq.push({ ny, nx, data.wave + 1 });
+					chk.insert({ ny, nx, data.wave + 1 });
+				}
 			}
 		}
+		else {
+			pq.push({ data.y, data.x, data.wave + 1 });
+		}
 	}
-
-	cur = next;
 }
 
 void solve()
@@ -56,8 +87,8 @@ void solve()
 	while (update) {
 		update = false;
 
-		wave();
 		ans++;
+		wave(ans);
 
 		int next = (cur + 1) % 2;
 		for (int y = 0; y < H; y++) {
@@ -66,6 +97,12 @@ void solve()
 					update = true;
 					break;
 				}
+			}
+		}
+		//next의 기록을 cur로 옮기기
+		for (int y = 0; y < H; y++) {
+			for (int x = 0; x < W; x++) {
+				map[cur][y][x] = map[next][y][x];
 			}
 		}
 	}
