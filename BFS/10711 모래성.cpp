826cacdf5dae28082156ad93e6b2ff335cb9e841
DESKTOP-L4QE09O\jsj03 <susan3722@snu.ac.kr>
2022-10-03 23:23:44 +0900

    Solution 3 - 관점을 바꾸자! -> 영향을 줄 수 있는 모래의 위치를 큐에 담기 - 처음에는 모든 모래를 큐에 담고 bfs를 1번 돌리면서 모래의 8방향 중 모래성이 있는 지점을 -1 하고 0이 되면 그 지점은 새로 영향을 줄 수 있는 지점이므로 큐에 담기 - 점점 유효한 모래들만 남게 되다가 더이상 변형이 없으면 큐에 삽입되지 않게 되고 큐가 비게 됨 - 큐가 모두 끝나면 영향이 모두 끝난 것 -> 더이상의 모양 변형은 없음 - 마지막에 담았던 구조체의 wave가 정답 -> while문 밖에 cur 선언

--- BFS/10711 모래성.cpp
+++ BFS/10711 모래성.cpp
@@ -1,29 +1,13 @@
 #include <iostream>
 #include <queue>
-#include <unordered_set>
 using namespace std;
 
 #define MAX 1000
 int H, W;
-char map[2][MAX][MAX];
-int cur;
+int map[MAX][MAX];
 
-struct STATUS {
-	int y; int x; int wave;
-	bool operator<(const STATUS& s) const {
-		return wave > s.wave;
-	}
-	bool operator==(const STATUS& s) const {
-		return (y == s.y) && (x == s.x) && (wave == s.wave);
-	}
-};
-struct HASH {
-	size_t operator()(const STATUS& s) const {
-		return s.y ^ s.x ^ s.wave;
-	}
-};
-priority_queue<STATUS> pq;
-unordered_set<STATUS, HASH> chk;
+struct STATUS { int y; int x; int wave; };
+queue<STATUS> q;
 
 
 void input()
@@ -31,83 +15,53 @@ void input()
 	cin >> H >> W;
 	for (int y = 0; y < H; y++) {
 		for (int x = 0; x < W; x++) {
-			cin >> map[0][y][x];
-			map[1][y][x] = map[0][y][x];
-			if (map[0][y][x] != '.') {
-				pq.push({ y, x, 1 });
-				chk.insert({ y, x, 1 });
+			char c;
+			cin >> c;
+			if (c == '.') {
+				map[y][x] = 0;
+			}
+			else {
+				map[y][x] = c - '0';
 			}
 		}
 	}
 }
 
-void wave(int n) //cur에서 next로 기록
+int bfs() 
 {
 	static int dx[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 	static int dy[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
 
-	int next = (cur + 1) % 2;
-	while (!pq.empty()) {
-		STATUS data = pq.top(); pq.pop();
-		if (data.wave == n + 1) {
-			pq.push(data);
-			return;
+	for (int y = 0; y < H; y++) { //영향을 줄 수 있는 파도의 위치를 담기
+		for (int x = 0; x < W; x++) {
+			if (map[y][x] == 0) {
+				q.push({ y, x, 0 });
+			}
 		}
+	}
 
-		int cnt = 0;
+	STATUS cur;
+	while (!q.empty()) {
+		cur = q.front(); q.pop();
+
 		for (int d = 0; d < 8; d++) {
-			int nx = data.x + dx[d];
-			int ny = data.y + dy[d];
-			if (map[cur][ny][nx] == '.') cnt++;
-		}
-		if (cnt >= map[cur][data.y][data.x] - '0') {
-			map[next][data.y][data.x] = '.';
-			//8방향 모래성 모두 pq에 넣기
-			for (int d = 0; d < 8; d++) {
-				int nx = data.x + dx[d];
-				int ny = data.y + dy[d];
-				auto it = chk.find({ ny, nx, data.wave + 1 });
-				if (it == chk.end() && map[cur][ny][nx] != '.') {
-					pq.push({ ny, nx, data.wave + 1 });
-					chk.insert({ ny, nx, data.wave + 1 });
+			int nx = cur.x + dx[d];
+			int ny = cur.y + dy[d];
+			if (nx < 0 || ny < 0 || nx >= W || ny >= H) continue;
+			if (map[ny][nx]) {
+				map[ny][nx]--;
+				if (map[ny][nx] == 0) { //무너졌다면 현재 위치를 큐에 넣기
+					q.push({ ny, nx, cur.wave + 1 });
 				}
 			}
 		}
-		else {
-			pq.push({ data.y, data.x, data.wave + 1 });
-		}
 	}
+	return cur.wave;
 }
 
 void solve()
 {
-	bool update = true;
-	int ans = 0;
-
-	while (update) {
-		update = false;
-
-		ans++;
-		wave(ans);
-
-		int next = (cur + 1) % 2;
-		for (int y = 0; y < H; y++) {
-			for (int x = 0; x < W; x++) {
-				if (map[next][y][x] != map[cur][y][x]) {
-					update = true;
-					break;
-				}
-			}
-		}
-		//next의 기록을 cur로 옮기기
-		for (int y = 0; y < H; y++) {
-			for (int x = 0; x < W; x++) {
-				map[cur][y][x] = map[next][y][x];
-			}
-		}
-	}
-
-	cout << ans - 1;
+	cout << bfs();
 }
 
 
