9181886f6b5a92224afdcedad80fd982c409f698
DESKTOP-L4QE09O\jsj03 <susan3722@snu.ac.kr>
2022-10-05 22:29:17 +0900

    Solution 2 - 3차원 map + shark 구조체를 unordered_map에 담아 사용 -> 4ms - map[][][0] : 현재 좌표에 존재하는 상어 번호 / map[][][1] : 냄새의 주인 상어 번호 / map[][][2] : 냄새의 양 - 기본적으로 solve 함수에서 냄새 spread -> 상어 이동 -> 냄새 줄어들기 로직으로 진행     - killed 변수를 두어서 M-1마리가 죽었을때 for문을 멈춤 -> 마지막 남는 것은 무조건 1번 상어이므로 1번상어인지 확인해줄 필요 X - 중요 전제**     - 한 칸에는 하나의 냄새만 찰 수 있다 & 냄새가 남아 있어도 갱신 가능하고 갱신되면 남아있는 냄새의 양에서 더해지는 것이 아니라 K로 갱신됨     - 상어 이동 즉시 상어를 죽일지 말지 결정할 수 있음 -> 이동하려는 칸에 있는 상어가 자신보다 작은 숫자면 죽음 -> 상어 번호 오름차순으로 move 진행하므로 비교문을 추가하지 않았음 - unordered_map에 <상어 번호, 상어 정보>를 mapping 시켜서 상어가 죽으면 map에서 아예 지워버림 - 4방향으로 2번 돌기 -> 처음에는 빈칸 찾고 두번째에는 자기 냄새가 있는 칸 찾기 / 한번의 for문에서 진행하면 안

--- 삼성 코테 기출/19237 어른 상어.cpp
+++ 삼성 코테 기출/19237 어른 상어.cpp
@@ -1,24 +1,17 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <unordered_map>
-#include <unordered_set>
-#include <list>
 using namespace std;
 
 #define MAXN 20
 int N, M, K;
-unordered_set<int> map_sn[MAXN][MAXN]; //좌표마다 상어 번호 저장
-
-struct SMELL { int n; int remain; };
-list<SMELL> map_sml[MAXN][MAXN]; //좌표마다 냄새 저장
+int map[MAXN][MAXN][3];
+int killed;
 
 struct SHARK {
-	int d; //현재 방향
-	int y, x;
-	int dir[4][4]; //[현재방향][우선순위 방향들]
+	int y, x, d;
+	int dir[4][4];
 };
-unordered_map<int, SHARK> shark_map; //<상어번호, 상어정보>
+unordered_map<int, SHARK> shark;
 
 
 void input()
@@ -29,143 +22,119 @@ void input()
 		for (int x = 0; x < N; x++) {
 			cin >> n;
 			if (n) {
-				map_sn[y][x].insert(n); //좌표에 상어 저장
-				shark_map.insert({ n, {} });
-				shark_map[n].y = y;
-				shark_map[n].x = x;
+				map[y][x][0] = n;
+				shark.insert({ n, {} });
+				shark[n].y = y; 
+				shark[n].x = x;
 			}
 		}
 	}
 	for (int i = 1; i <= M; i++) {
 		cin >> n;
-		n--;
-		shark_map[i].d = n;
+		--n; shark[i].d = n;
 	}
 	for (int i = 1; i <= M; i++) {
 		for (int y = 0; y < 4; y++) {
 			for (int x = 0; x < 4; x++) {
 				cin >> n;
-				n--;
-				shark_map[i].dir[y][x] = n;
+				--n; shark[i].dir[y][x] = n;
 			}
 		}
 	}
 }
 
-void spread_smell() //모든 상어가 자신의 위치에서 자신의 냄새 뿌림
+void spread() //모든 상어가 자신의 자리에서 냄새를 뿌림
 {
-	for (auto it = shark_map.begin(); it != shark_map.end(); it++) {
-		SMELL tmp = { it->first, K };
-		map_sml[(it->second).y][(it->second).x].push_back(tmp);
+	for (int y = 0; y < N; y++) {
+		for (int x = 0; x < N; x++) {
+			if (map[y][x][0]) { //상어가 존재
+				map[y][x][1] = map[y][x][0];
+				map[y][x][2] = K;
+			}
+		}
 	}
 }
 
-void shark_move() //상어의 이동 : 빈칸 -> 자신의 냄새가 있는 칸
+void move_shark()
 {
 	static int dx[] = { 0, 0, -1, 1 };
 	static int dy[] = { -1, 1, 0, 0 };
 
-	for (auto it = shark_map.begin(); it != shark_map.end(); it++) {
-		SHARK& tmp = it->second;
-
-		//현재 방향 기준으로 우선순위 상하좌우 확인 -> 빈칸 있으면 바로 가기
+	for (int i = 1; i <= M; i++) {
+		if (shark.find(i) == shark.end()) continue; //죽은 상어
+		SHARK& tmp = shark[i];
+		map[tmp.y][tmp.x][0] = 0;
+		
 		bool flag = false;
-		for (int i = 0; i < 4; i++) {
-			int nd = tmp.dir[tmp.d][i];
+		for (int d = 0; d < 4; d++) { //1. 우선순위로 돌면서 아무 냄새 없는 칸 
+			int nd = tmp.dir[tmp.d][d];
 			int nx = tmp.x + dx[nd];
 			int ny = tmp.y + dy[nd];
-			if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-			if (map_sml[ny][nx].empty()) { //상어 이동
-				map_sn[tmp.y][tmp.x].erase(it->first);
-				map_sn[ny][nx].insert(it->first);
-				tmp.x = nx; 
+			if (nx < 0 || ny < 0 || nx >= N || ny >= N || map[ny][nx][1]) continue;
+			if (map[ny][nx][0]) { //상어가 있음 -> 나중에 들어온 i가 숫자가 더 클 것이므로 못들어감 -> 현재 상어 죽음
+				shark.erase(i);
+				killed++;
+				flag = true;
+				break;
+			}
+			else {
+				map[ny][nx][0] = i;
 				tmp.y = ny;
+				tmp.x = nx;
 				tmp.d = nd;
-				flag = true; break;
+				flag = true;
+				break;
 			}
 		}
-		if (!flag) { //빈칸을 찾지 못했음 -> 상하좌우 한번 더 확인하고 자기 냄새가 있는 칸으로 이동
-			for (int i = 0; i < 4; i++) {
-				int nd = tmp.dir[tmp.d][i];
+		if (!flag) { //2. 우선순위로 돌면서 자기 냄새가 있는 칸 찾기
+			for (int d = 0; d < 4; d++) { 
+				int nd = tmp.dir[tmp.d][d];
 				int nx = tmp.x + dx[nd];
 				int ny = tmp.y + dy[nd];
 				if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-				for (SMELL s : map_sml[ny][nx]) {
-					if (s.n == it->first) {
-						map_sn[tmp.y][tmp.x].erase(it->first);
-						map_sn[ny][nx].insert(it->first);
-						tmp.x = nx;
-						tmp.y = ny;
-						tmp.d = nd;
-						flag = true; break;
-					}
+				if (map[ny][nx][1] != i) continue; //자신의 냄새가 아닌 방 제외
+				if (map[ny][nx][0]) { //상어가 있음 -> 못들어감 -> 현재 상어 죽음
+					shark.erase(i);
+					killed++;
+					break;
 				}
-				if (flag) break;
+				else { //들어감
+					map[ny][nx][0] = i;
+					tmp.y = ny;
+					tmp.x = nx;
+					tmp.d = nd;
+					break;
+				}
 			}
 		}
 	}
 }
 
-void disappear_smell() //냄새에서 1초씩 뺀 뒤 0이 된 냄새는 제거
+void disappear()
 {
 	for (int y = 0; y < N; y++) {
 		for (int x = 0; x < N; x++) {
-			if (map_sml[y][x].empty()) continue;
-			for (SMELL& s : map_sml[y][x]) s.remain -= 1;
-			
-			auto it = map_sml[y][x].begin();
-			while (it != map_sml[y][x].end()) {
-				if (it->remain == 0) map_sml[y][x].erase(it++);
-				else it++;
+			if (map[y][x][2] > 0) { //냄새가 있음
+				map[y][x][2]--;
+				if (map[y][x][2] == 0) {
+					map[y][x][1] = 0; //냄새 주인 없애기
+				}
 			}
 		}
 	}
-}
-
-void remove_shark()
-{
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < N; x++) {
-			if (map_sn[y][x].empty() || map_sn[y][x].size() == 1) continue;
-			vector<int> tmp;
-			for (int n : map_sn[y][x]) tmp.push_back(n);
-			sort(tmp.begin(), tmp.end(), greater<int>());
-			for (int i = 0; i < tmp.size() - 1; i++) {
-				map_sn[y][x].erase(tmp[i]);
-				shark_map.erase(tmp[i]);
-			}
-		}
-	}
-}
-
-bool end_check()
-{
-	//격자에 1번 상어만 남았는지 확인
-	bool flag = false; //1번 상어 발견하면 true
-	int cnt = 0; //상어 개수 카운트
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < N; x++) {
-			if (map_sn[y][x].empty()) continue;
-			cnt += map_sn[y][x].size();
-			if (map_sn[y][x].find(1) != map_sn[y][x].end()) flag = true;
-		}
-	}
-	if (cnt == 1 && flag) return true;
-	else return false;
 }
 
 void solve()
 {
 	for (int t = 1; t <= 1000; t++) {
-		spread_smell();
-		shark_move();
-		disappear_smell();
-		remove_shark();
+		spread();
+		move_shark();
+		disappear();
 
-		if (end_check()) { 
-			cout << t; 
-			return; 
-		}	
+		if (killed == M - 1) {
+			cout << t;
+			return;
+		}
 	}
 	cout << -1;
 }
