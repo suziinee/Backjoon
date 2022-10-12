8de6c4ebea76348e4c7c2086eb21c38e66d18cb1
DESKTOP-L4QE09O\jsj03 <susan3722@snu.ac.kr>
2022-10-12 10:35:06 +0900

    Solution 3 - 선수를 뽑는 것이 아니라 팀을 뽑는 것으로 관점을 바꿈 / solution 2 방법보다 훨씬 빠름 - 1번부터 N번까지 선수를 team[0], team[1]에 배정하는 경우의 수     - 이때 vector를 사용해서 push_back으로 n번 선수를 배정하고 dfs를 호출한 후 돌아오면 pop_back을 해서 되돌림 - get_score() : 점수를 구하려는 팀의 인원이 0이면 한명이 추가되어도 점수는 아직 0이므로 0 리턴

--- DFS/15661 링크와 스타트.cpp
+++ DFS/15661 링크와 스타트.cpp
@@ -1,67 +1,60 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 #define MAXN 20
 int N;
-int map[MAXN][MAXN];
-int team1[MAXN]; int team2[MAXN];
-int pick[MAXN]; //1과 0으로 구분
+int map[MAXN + 1][MAXN + 1]; //1번부터 사용
 int ans = 0x7fffffff;
+vector<int> team[2];
 
 
-void input() 
+void input()
 {
 	cin >> N;
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < N; x++) {
+	for (int y = 1; y <= N; y++) {
+		for (int x = 1; x <= N; x++) {
 			cin >> map[y][x];
 		}
 	}
 }
 
-int check()
+int get_score(int t, int n) //n번 선수를 team[t]에 넣었을 때 점수
 {
-	int t1 = 0; int t2 = 0;
-	for (int i = 0; i < N; i++) {
-		if (pick[i]) team1[t1++] = i;
-		else team2[t2++] = i;
+	if (team[t].size() == 0) return 0;
+	int ret = 0;
+	for (int m : team[t]) {
+		ret += map[m][n] + map[n][m];
 	}
-
-	int sum1 = 0; int sum2 = 0;
-	for (int i = 0; i < t1; i++) {
-		for (int j = i + 1; j < t1; j++) {
-			sum1 += map[team1[i]][team1[j]] + map[team1[j]][team1[i]];
-		}
-	}
-	for (int i = 0; i < t2; i++) {
-		for (int j = i + 1; j < t2; j++) {
-			sum2 += map[team2[i]][team2[j]] + map[team2[j]][team2[i]];
-		}
-	}
-
-	return abs(sum1 - sum2);
+	return ret;
 }
 
-void dfs(int n, int s, int diff) 
+void dfs(int n, int start, int link)
 {
-	if (diff < ans) ans = diff;
-	if (n == N - 1) return;
+	if (n > N) {
+		if (!team[0].empty() && !team[1].empty() && abs(start - link) < ans) {
+			ans = abs(start - link);
+		}
+		return;
+	}
 
-	for (int i = s; i < N; i++) { //i번째를 뽑아서 start에 넣는 것
-		pick[i] = 1;
-		dfs(n + 1, i + 1, check());
-		pick[i] = 0;
+	for (int t = 0; t < 2; t++) {
+		int score = get_score(t, n);
+		team[t].push_back(n);
+		dfs(n + 1, start + (t == 0 ? score : 0), link + (t == 1 ? score : 0));
+		team[t].pop_back();
 	}
 }
 
-void solve() 
+void solve()
 {
-	dfs(0, 0, 0x7fffffff);
+	dfs(1, 0, 0); //1번부터 N번까지 team[0] or team[1]에 배정
 	cout << ans;
 }
 
 
-int main(void)
+int main()
 {
 	input();
 	solve();
