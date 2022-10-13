7c70dc6b1aea794db45b5d58925b3bfddc9a4bef
DESKTOP-L4QE09O\jsj03 <susan3722@snu.ac.kr>
2022-10-13 11:57:55 +0900

    Solution 3 - 자료구조, 정렬 하나도 사용하지 않은 방법 - 학생이 입력되는 순서대로 자리를 배치해야 하므로 stu 배열은 입력 순서대로 받고 학생 구조체에 번호를 넣어놓기 -> stu 배열을 0번부터 돌리면 됨 - 학생 자리 배정 시 비어있지 않은 모든 map을 탐색하며 get_point 함수로 점수를 받아서 최고 점수인 자리로 배정     - get_point() : 좋아하는 학생과 비어있는 칸 모두 합해도 4칸이므로 좋아하는 학생일 경우 +10점, 비어있는 칸은 +1점을 주면 점수가 over되지 않게 자리의 우선순위 판단 가능     - assign() : 학생 자리 배정 시 map과 stu 배열의 구조체 sync 맞추기 - 학생별로 만족도 조사     - get_score() : 0명이면 0점, 1명부터 10배씩 점수가 올라가므로 0점일 때 증가와 그 외 점수일때 증가를 다르게 하면 간단하게 점수를 구할 수 있음

--- 삼성 코테 기출/21608 상어 초등학교.cpp
+++ 삼성 코테 기출/21608 상어 초등학교.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
 using namespace std;
 
 #define MAXN 20
@@ -10,107 +7,91 @@ int map[MAXN][MAXN]; //앉은 학생의 번호 기록
 
 struct STUDENT {
 	int n;
+	int y, x;
 	int pri[4];
 };
-vector<STUDENT> student;
-unordered_map<int, STUDENT*> ht;
+STUDENT stu[MAXN * MAXN]; //입력 순서대로
 
-struct STATUS {
-	int fav; //인접한 칸 중 좋아하는 학생이 앉은 칸
-	int blk; //인접한 칸 중 비어있는 칸
-	int y, x;
-	bool operator<(const STATUS& s) const {
-		if (fav == s.fav) {
-			if (blk == s.blk) {
-				if (y == s.y) {
-					return x < s.x;
-				}
-				return y < s.y;
-			}
-			return blk > s.blk;
-		}
-		return fav > s.fav;
-	}
-};
+const int dx[] = { 0, 1, 0, -1 };
+const int dy[] = { -1, 0, 1, 0 };
 
 
 void input()
 {
 	cin >> N;
-	int n, p1, p2, p3, p4;
 	for (int i = 0; i < N * N; i++) {
-		cin >> n >> p1 >> p2 >> p3 >> p4;
-		student.push_back({ n, {p1, p2, p3, p4} });
-	}
-	for (int i = 0; i < N * N; i++) {
-		ht.insert({ student[i].n, &student[i] });
+		cin >> stu[i].n >> stu[i].pri[0] >> stu[i].pri[1] >> stu[i].pri[2] >> stu[i].pri[3];
 	}
 }
 
-void assign(STUDENT& s)
+int get_point(int y, int x, int i) //(y, x)에 stu[i].n번 학생이 앉았을 때 가산점
 {
-	static int dx[] = { 0, 1, 0, -1 };
-	static int dy[] = { -1, 0, 1, 0 };
-
-	vector<STATUS> st;
-
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < N; x++) {
-			if (map[y][x]) continue;
-			int fav = 0; int blk = 0;
-			for (int d = 0; d < 4; d++) {
-				int nx = x + dx[d];
-				int ny = y + dy[d];
-				if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-				if (map[ny][nx] == 0) blk++;
-				for (int n : s.pri) {
-					if (map[ny][nx] == n) { fav++; break; }
+	int ret = 0;
+	for (int d = 0; d < 4; d++) {
+		int nx = x + dx[d];
+		int ny = y + dy[d];
+		if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+		if (map[ny][nx] == 0) {
+			ret += 1;
+		}
+		else {
+			for (int f : stu[i].pri) {
+				if (map[ny][nx] == f) {
+					ret += 10;
+					break;
 				}
 			}
-			st.push_back({ fav, blk, y, x });
 		}
 	}
-	
-	sort(st.begin(), st.end());
-	map[st[0].y][st[0].x] = s.n;
+	return ret;
 }
 
-int get_score()
+void assign(int y, int x, int i) //(y, x)에 stu[i].n번 학생 앉히기
 {
-	static int dx[] = { 0, 1, 0, -1 };
-	static int dy[] = { -1, 0, 1, 0 };
+	map[y][x] = stu[i].n;
+	stu[i].y = y; stu[i].x = x;
+}
 
-	int sum = 0;
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < N; x++) {
-			int cnt = 0;
-			for (int d = 0; d < 4; d++) {
-				int nx = x + dx[d];
-				int ny = y + dy[d];
-				if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-				for (int n : ht[map[y][x]] -> pri) {
-					if (map[ny][nx] == n) { cnt++; break; }
-				}
-			}
-			switch (cnt) {
-			case 0: break;
-			case 1: sum += 1; break;
-			case 2: sum += 10; break;
-			case 3: sum += 100; break;
-			case 4: sum += 1000; break;
+int get_score(int i) //stu[i].n번 학생의 만족도
+{
+	int ret = 0;
+	for (int d = 0; d < 4; d++) {
+		int nx = stu[i].x + dx[d];
+		int ny = stu[i].y + dy[d];
+		if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+		for (int f : stu[i].pri) {
+			if (map[ny][nx] == f) {
+				if (ret == 0) ++ret;
+				else ret *= 10;
 			}
 		}
 	}
-	return sum;
+	return ret;
 }
 
 void solve()
 {
-	map[1][1] = student[0].n;
-	for (int i = 1; i < N * N; i++) {
-		assign(student[i]);
+	for (int i = 0; i < N * N; i++) {
+		//stu[i].n번 학생이 앉을 자리 찾기
+		int candy, candx, point = -1;
+		for (int y = 0; y < N; y++) {
+			for (int x = 0; x < N; x++) {
+				if (map[y][x]) continue;
+				int ret = get_point(y, x, i);
+				if (ret > point) {
+					point = ret;
+					candy = y; candx = x;
+				}
+			}
+		}
+		assign(candy, candx, i);
 	}
-	cout << get_score();
+
+	int ans = 0;
+	for (int i = 0; i < N * N; i++) {
+		ans += get_score(i);
+	}
+	cout << ans;
 }
 
 
