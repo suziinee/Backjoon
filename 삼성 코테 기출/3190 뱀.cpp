c92bc070bec7bfa9648391624016985c8acdee4b
DESKTOP-L4QE09O\jsj03 <susan3722@snu.ac.kr>
2022-10-13 23:38:58 +0900

    Solution 3 - deque에 뱀의 좌표를 넣고 map에 표시하는 것과의 sync를 맞춰서 이토록 간단하게 짤 수 있었다!

--- 삼성 코테 기출/3190 뱀.cpp
+++ 삼성 코테 기출/3190 뱀.cpp
@@ -1,125 +1,86 @@
 #include <iostream>
-#include <vector>
+#include <deque>
 #include <queue>
-#include <algorithm>
 using namespace std;
 
-int n, k, l;
-struct AXIS { int y; int x; int d; };
-struct MOVE { int t; int rot; }; //왼쪽은 0, 오른쪽은 1
-vector<MOVE> mov;
-int map[101][101]; //사과는 2, 뱀의 몸은 1
-int ans;
+#define MAXN 100
+#define MAXL 100
+int N, K, L;
+int map[MAXN][MAXN]; //빈칸 0, 사과 1, 뱀 2
 
+struct AXIS { int y, x; };
+deque<AXIS> snake;
+int dir = 1; //뱀은 처음에 오른쪽
+
+struct ORDER { int t, d; }; //1은 왼쪽, 2는 오른쪽
+queue<ORDER> oq;
+
+#define BLANK (0)
+#define APPLE (1)
+#define SNAKE (2)
 
 void input()
 {
-	cin >> n >> k;
-	for (int i = 0; i < k; i++) {
-		int x, y;
+	cin >> N >> K;
+	int y, x;
+	for (int i = 0; i < K; i++) {
 		cin >> y >> x;
-		map[y][x] = 2;
+		--y; --x;
+		map[y][x] = APPLE;
 	}
-	cin >> l;
-	for (int i = 0; i < l; i++) {
-		int t; char c; 
+	cin >> L;
+	int t; char c;
+	for (int i = 0; i < L; i++) {
 		cin >> t >> c;
-		if (c == 'L') {
-			mov.push_back({ t, 0 });
-		}
-		else {
-			mov.push_back({ t, 1 });
-		}
+		if (c == 'L') oq.push({ t, 1 });
+		else oq.push({ t, 2 });
 	}
-	
-	//계속 돌게 하기 위해 마지막 mov 추가
-	mov.push_back({ 0x7fffffff, 2 });
-	l += 1;
 }
 
-
-void output()
-{
-	cout << ans;
-}
-
-
-const int dx[] = { 0, 1, 0, -1 };
-const int dy[] = { -1, 0, 1, 0 };
 void solve()
 {
-	queue<AXIS> q;
-	AXIS head = { 1, 1, 1 };
-	AXIS tail = { 1, 1, 1 };
-	map[1][1] = 1;
+	static int dx[] = { 0, 1, 0, -1 };
+	static int dy[] = { -1, 0, 1, 0 };
 
-	for (int i = 0; i < l; i++) { //모든 이동경로에 대해 탐색
-		
-		while (ans < mov[i].t) {
-			//0. 이동 및 시간 증가
-			int nx = head.x + dx[head.d];
-			int ny = head.y + dy[head.d];
-			ans++;
+	snake.push_back({ 0, 0 });
+	map[0][0] = SNAKE;
+	int time = 0;
+	while (true) {
 
-			//0-0. 범위를 벗어날 경우 게임 종료
-			if (nx<1 || ny<1 || nx>n || ny>n) return;
-			//0-1. 자기 자신과 부딪힐 경우 게임 종료
-			if (map[ny][nx] == 1) return;
-
-			//0-2. 종료 조건을 통과했다면 head 갱신
-			head = { ny, nx, head.d };
-			
-			//1. 사과가 있는지 확인
-			if (map[ny][nx] == 2) {
-				//1-1. 사과를 먹음
-				map[ny][nx] = 1;
-				//1-1-0. head 갱신, tail은 그대로
-				continue;
+		//oq가 비어있지 않다면 방향 전환
+		if (!oq.empty() && oq.front().t == time) {
+			if (oq.front().d == 1) { //왼쪽 회전
+				dir = (dir + 3) % 4;
 			}
-			else {
-				//1-2. 사과가 없다면 head가 머리 확장 + 꼬리 지우기
-				map[ny][nx] = 1;
-				map[tail.y][tail.x] = 0;
-				
-				//1-3. 꼬리가 자신의 방향으로 이동
-				//1-3-0. 이때 꼬리가 방향을 전환해야 할지 판단
-				if (!q.empty()) {
-					AXIS tmp = q.front();
-					if (tail.x == tmp.x&&tail.y == tmp.y) { //1-3-1. tail 방향 전환 후 이동, 갱신
-						tail.d = tmp.d;
-						tail.x = tail.x + dx[tail.d];
-						tail.y = tail.y + dy[tail.d];
-						q.pop();
-						continue;
-					}
-				}
-				//1-3-2. 방향 전환하지 않고 이동, 갱신
-				tail.x = tail.x + dx[tail.d];
-				tail.y = tail.y + dy[tail.d];
+			else { //오른쪽 회전
+				dir = (dir + 1) % 4;
 			}
+			oq.pop();
 		}
 
-		//2. head 방향 전환
-		if (mov[i].rot == 0) { //2-1-0. 왼쪽 회전
-			switch (head.d) {
-				case 0: head.d = 3; break;
-				case 1: head.d = 0; break;
-				case 2: head.d = 1; break;
-				case 3: head.d = 2; break;
-			}
+		//뱀 머리 늘이기
+		int nx = snake.front().x + dx[dir];
+		int ny = snake.front().y + dy[dir];
+		if (nx < 0 || ny < 0 || nx >= N || ny >= N) { ++time; break; }
+		if (map[ny][nx] == SNAKE) { ++time; break; }
+		snake.push_front({ ny, nx });
+
+		//사과 있으면 먹고 아니면 그대로
+		if (map[ny][nx] == APPLE) {
+			map[ny][nx] = SNAKE;
 		}
-		else { //2-1-1. 오른쪽 회전
-			switch (head.d) {
-				case 0: head.d = 1; break;
-				case 1: head.d = 2; break;
-				case 2: head.d = 3; break;
-				case 3: head.d = 0; break;
-			}
+		else if (map[ny][nx] == BLANK) {
+			map[ny][nx] = SNAKE;
+			int ty = snake.back().y;
+			int tx = snake.back().x;
+			map[ty][tx] = BLANK;
+			snake.pop_back();
 		}
 
-		//3. head가 방향 전환된 곳을 queue에 저장
-		q.push({ head.y, head.x, head.d }); //3-1. 이때 dir은 전환된 다음 방향
+		//시간 증가
+		++time;
 	}
+	cout << time;
 }
 
 
@@ -127,6 +88,5 @@ int main()
 {
 	input();
 	solve();
-	output();
 	return 0;
 }
