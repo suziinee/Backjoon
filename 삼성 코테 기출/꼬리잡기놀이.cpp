d061e48ef33cfab87556cc96448d101f08ca9d90
DESKTOP-L4QE09O\jsj03 <susan3722@snu.ac.kr>
2022-10-13 20:33:19 +0900

    Solution 2 - deque을 사용해서 풀이 (뱀 문제와 유사) - deque에 몸의 좌표들을 모두 넣으면 움직일때 머리와 꼬리를 더해주거나 빼주기만 하면 되어서 매우 쉬워짐!!** - team 번호 1~5, road 6, 초반 머리 7, 몸통 8, 꼬리 9로 인덱싱 해줘서 dfs 시 chk 배열을 사용하지 않아도 되게 만듦 - init_team() : 머리를 탐색하여 바로 뒤 body까지 찾아준 뒤 첫번째 body부터 dfs 호출 -> 머리 옆에 body, tail 모두 있을 경우를 대비 - team_move() : team의 dir에 따라 꼬리 자르고 머리 추가하는 순서대로 진행 - get_score() : 맞은 공의 위치와 팀번호를 가지고 점수를 구해서 합산하고 팀의 방향을 바꿔줌     - 공이 맞은 위치가 줄에서 몇 번째인지 알기 위해서 아예 머리부터 deque을 iterator로 순서대로 확인함**     - 혼자 생각한 방법에서는 시작 지점부터 4방향 확인해서 목표 지점까지 찾아가거나 하는 방법이었는데 훨씬 효율적     - 단 뒷부분이 머리일 경우에는 전체 개수를 한번 세어줘야함 -> rbegin, rend 사용하면 편리

--- 삼성 코테 기출/꼬리잡기놀이.cpp
+++ 삼성 코테 기출/꼬리잡기놀이.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
-#include <algorithm>
-#include <queue>
+#include <deque>
 using namespace std;
 
 #define MAXN 20
 int N, M, K;
 int map[MAXN][MAXN];
-
-struct TEAM {
-	int sy, sx;
-	int ey, ex;
-	int score;
-};
-TEAM team[5];
+int ans;
 
 struct AXIS { int y, x; };
-queue<AXIS> q;
-
-int team_num[5][5] = {
-	{0, 1, 2, 3, 4}, {0, 11, 22, 33, 44}, {0, 111, 222, 333, 444}, {0, 1111, 2222, 3333, 4444},
-	{0, 11111, 22222, 33333, 44444}
+struct TEAM {
+	int dir = 1; //1이면 앞쪽이 머리, 0이면 뒤쪽이 머리
+	deque<AXIS> chain;
 };
+TEAM team[5 + 1]; //1번~5번
 
 const int dx[] = { 0, 1, 0, -1 };
 const int dy[] = { -1, 0, 1, 0 };
 
+#define ROAD (6)
+#define HEAD (7)
+#define BODY (8)
+#define TAIL (9)
 
 void input()
 {
@@ -32,178 +28,127 @@ void input()
 	for (int y = 0; y < N; y++) {
 		for (int x = 0; x < N; x++) {
 			cin >> map[y][x];
+			if (map[y][x] == 4) map[y][x] = ROAD;
+			else if (map[y][x] == 1) map[y][x] = HEAD;
+			else if (map[y][x] == 2) map[y][x] = BODY;
+			else if (map[y][x] == 3) map[y][x] = TAIL;
 		}
 	}
 }
 
-void dfs(int y, int x, bool(*chk)[MAXN], int m)
+void dfs(int y, int x, int m)
 {
-	chk[y][x] = true;
+	map[y][x] = m;
+	team[m].chain.push_back({ y, x });
 	for (int d = 0; d < 4; d++) {
 		int nx = x + dx[d];
 		int ny = y + dy[d];
 		if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-		if (map[ny][nx] == 0 || chk[ny][nx]) continue;
-		if (map[ny][nx] == 4) {
-			dfs(ny, nx, chk, m);
+		if (map[ny][nx] == BODY) {
+			dfs(ny, nx, m);
 		}
-		else if (map[ny][nx] == 1) { //sy, sx
-			team[m].sy = ny;
-			team[m].sx = nx;
-			map[ny][nx] = team_num[m][1];
-		}
-		else if (map[ny][nx] == 3) { //ey, ex
-			team[m].ey = ny;
-			team[m].ex = nx;
-			map[ny][nx] = team_num[m][3];
-		}
-	}
-}
-
-void change_two(int t) //2를 팀 번호에 맞게 바꿔줌
-{
-	int cy = team[t].sy;
-	int cx = team[t].sx;
-	while (true) {
-		for (int d = 0; d < 4; d++) {
-			int nx = cx + dx[d];
-			int ny = cy + dy[d];
-			if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-			if (map[ny][nx] == 2) {
-				map[ny][nx] = team_num[t][2];
-				cy = ny;
-				cx = nx;
-				break;
-			}
-			else if (map[ny][nx] == team_num[t][3]) {
-				return;
-			}
+		else if (map[ny][nx] == TAIL) {
+			map[ny][nx] = m;
+			team[m].chain.push_back({ ny, nx });
 		}
 	}
 }
 
 void init_team()
 {
-	//4를 발견하면 flood fill 진행
-	bool chk[MAXN][MAXN] = { false, };
-	int m = -1; //팀 번호 : 0번~M-1번
+	int m = 0;
 	for (int y = 0; y < N; y++) {
 		for (int x = 0; x < N; x++) {
-			if (map[y][x] == 4 && chk[y][x] == false) {
-				++m;
-				dfs(y, x, chk, m);
+			if (map[y][x] != HEAD) continue;
+			map[y][x] = ++m;
+			team[m].chain.push_back({ y, x });
+			for (int d = 0; d < 4; d++) { //body 하나 찾고 dfs 보내기
+				int nx = x + dx[d];
+				int ny = y + dy[d];
+				if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+				if (map[ny][nx] == BODY) {
+					dfs(ny, nx, m);
+					break;
+				}
 			}
 		}
 	}
-	for (int i = 1; i < M; i++) {
-		change_two(i);
+}
+
+void team_move(int t) //t번 팀 이동
+{
+	//꼬리 자르기
+	int ty, tx;
+	if (team[t].dir) { //앞쪽이 머리, 뒤쪽이 꼬리
+		ty = team[t].chain.back().y;
+		tx = team[t].chain.back().x;
+		map[ty][tx] = ROAD;
+		team[t].chain.pop_back();
+	}
+	else { //뒤쪽이 머리, 앞쪽이 꼬리
+		ty = team[t].chain.front().y;
+		tx = team[t].chain.front().x;
+		map[ty][tx] = ROAD;
+		team[t].chain.pop_front();
+	}
+
+	//머리 추가
+	int y, x;
+	if (team[t].dir) { //앞쪽이 머리, 뒤쪽이 꼬리
+		y = team[t].chain.front().y;
+		x = team[t].chain.front().x;
+	}
+	else { //뒤쪽이 머리, 앞쪽이 꼬리
+		y = team[t].chain.back().y;
+		x = team[t].chain.back().x;
+	}
+	for (int d = 0; d < 4; d++) {
+		int nx = x + dx[d];
+		int ny = y + dy[d];
+		if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+		if (map[ny][nx] == ROAD) {
+			if (team[t].dir) {
+				team[t].chain.push_front({ ny, nx });
+			}
+			else {
+				team[t].chain.push_back({ ny, nx });
+			}
+			map[ny][nx] = t;
+			return;
+		}
 	}
 }
 
-void team_move(int t)
+void get_score(int y, int x, int t)
 {
-	int hy = team[t].sy;
-	int hx = team[t].sx;
-	int ty = team[t].ey;
-	int tx = team[t].ex;
-
-	//1. head가 주변의 4로 이동
-	for (int d = 0; d < 4; d++) {
-		int nx = hx + dx[d];
-		int ny = hy + dy[d];
-		if (map[ny][nx] == 4) {
-			map[ny][nx] = team_num[t][1];
-			team[t].sy = ny;
-			team[t].sx = nx;
-			break;
-		}
-	}
-	//2. head가 있던 자리 2로 채우기
-	map[hy][hx] = team_num[t][2];
-	//3. tail은 주변의 2로 이동
-	for (int d = 0; d < 4; d++) {
-		int nx = tx + dx[d];
-		int ny = ty + dy[d];
-		if (map[ny][nx] == team_num[t][2]) {
-			map[ny][nx] = team_num[t][3];
-			team[t].ey = ny;
-			team[t].ex = nx;
-			break;
-		}
-	}
-	//4. tail 있던 곳 4로 채우기
-	map[ty][tx] = 4;
-}
-
-int get_turn(int t, int y, int x) //(y, x)가 t번 팀에서 몇번째인지 찾기
-{
-	bool chk[MAXN][MAXN] = { false, };
 	int turn = 1;
-	int cy = team[t].sy;
-	int cx = team[t].sx;
-	chk[cy][cx] = true;
-	while (!(cy == y && cx == x)) {
-		for (int d = 0; d < 4; d++) {
-			int nx = cx + dx[d];
-			int ny = cy + dy[d];
-			if (nx < 0 || ny < 0 || nx >= N || ny >= N || chk[ny][nx]) continue;
-			if (map[ny][nx] == team_num[t][2] || map[ny][nx] == team_num[t][3]) {
-				chk[ny][nx] = true;
-				cy = ny;
-				cx = nx;
-				++turn;
-				break;
-			}
+	if (team[t].dir) { //머리부터 찾기
+		for (auto it = team[t].chain.begin(); it != team[t].chain.end(); it++) {
+			if (it->y == y && it->x == x) break;
+			turn++;
 		}
+		team[t].dir = 0; //방향 바꿔주기
+		ans += turn * turn;
 	}
-	return turn;
-}
-
-void head_tail_swap(int t) //t번 팀의 머리와 꼬리 바꿔주기
-{
-	int tmpx = team[t].sx;
-	int tmpy = team[t].sy;
-	team[t].sx = team[t].ex;
-	team[t].sy= team[t].ey;
-	team[t].ex = tmpx;
-	team[t].ey = tmpy;
-}
-
-void round_sub(int y, int x) //몇번 팀인지 탐색해서 점수 올려주고 머리 꼬리 방향 바꿔줌
-{
-	if (map[y][x] < 10) { //1번팀
-		int turn = get_turn(0, y, x);
-		team[0].score += turn * turn;
-		head_tail_swap(0);
-	}
-	else if (map[y][x] < 100) { //2번팀
-		int turn = get_turn(1, y, x);
-		team[1].score += turn * turn;
-		head_tail_swap(1);
-	}
-	else if (map[y][x] < 1000) { //3번팀
-		int turn = get_turn(2, y, x);
-		team[2].score += turn * turn;
-		head_tail_swap(2);
-	}
-	else if (map[y][x] < 10000) { //4번팀
-		int turn = get_turn(3, y, x);
-		team[3].score += turn * turn;
-		head_tail_swap(3);
-	}
-	else if (map[y][x] < 100000) { //5번팀
-		int turn = get_turn(4, y, x);
-		team[4].score += turn * turn;
-		head_tail_swap(4);
+	else { //머리부터 찾은 후 꼬리까지 count
+		int count = 0;
+		for (auto it = team[t].chain.begin(); it != team[t].chain.end(); it++) {
+			count++;
+		}
+		for (auto it = team[t].chain.begin(); it != team[t].chain.end(); it++) {
+			if (it->y == y && it->x == x) break;
+			turn++;
+		}
+		team[t].dir = 1;
+		ans += (count - turn + 1) * (count - turn + 1);
 	}
 }
 
 void round1(int r) //r번째 행을 0번 열부터 탐색
 {
 	for (int x = 0; x < N; x++) {
-		int n = map[r][x] % 10;
-		if (n == 1 || n == 2 || n == 3) {
-			round_sub(r, x);
+		if (map[r][x] != 0 && map[r][x] != ROAD) {
+			get_score(r, x, map[r][x]);
 			return;
 		}
 	}
@@ -211,12 +156,11 @@ void round1(int r) //r번째 행을 0번 열부터 탐색
 
 void round3(int r) //r번째 행을 N-1번 열부터 탐색
 {
-	r -= 2 * N;
+	r %= N;
 	r = N - 1 - r;
 	for (int x = N - 1; x >= 0; x--) {
-		int n = map[r][x] % 10;
-		if (n == 1 || n == 2 || n == 3) {
-			round_sub(r, x);
+		if (map[r][x] != 0 && map[r][x] != ROAD) {
+			get_score(r, x, map[r][x]);
 			return;
 		}
 	}
@@ -224,11 +168,10 @@ void round3(int r) //r번째 행을 N-1번 열부터 탐색
 
 void round2(int c) //c번째 열을 N-1번째 행부터 탐색
 {
-	c -= N;
+	c %= N;
 	for (int y = N - 1; y >= 0; y--) {
-		int n = map[y][c] % 10;
-		if (n == 1 || n == 2 || n == 3) {
-			round_sub(y, c);
+		if (map[y][c] != 0 && map[y][c] != ROAD) {
+			get_score(y, c, map[y][c]);
 			return;
 		}
 	}
@@ -236,12 +179,11 @@ void round2(int c) //c번째 열을 N-1번째 행부터 탐색
 
 void round4(int c) //c번째 열을 0번째 행부터 탐색
 {
-	c -= 3 * N;
+	c %= N;
 	c = N - 1 - c;
 	for (int y = 0; y < N; y++) {
-		int n = map[y][c] % 10;
-		if (n == 1 || n == 2 || n == 3) {
-			round_sub(y, c);
+		if (map[y][c] != 0 && map[y][c] != ROAD) {
+			get_score(y, c, map[y][c]);
 			return;
 		}
 	}
@@ -249,10 +191,10 @@ void round4(int c) //c번째 열을 0번째 행부터 탐색
 
 void solve()
 {
-	init_team();
+	init_team(); //dfs
 	for (int i = 0; i < K; i++) {
 		//팀 이동
-		for (int t = 0; t < M; t++) {
+		for (int t = 1; t <= M; t++) {
 			team_move(t);
 		}
 		//공이 던져짐
@@ -261,11 +203,6 @@ void solve()
 		else if (round >= N && round < 2 * N) round2(round);
 		else if (round >= 2 * N && round < 3 * N) round3(round);
 		else if (round >= 3 * N && round < 4 * N) round4(round);
-	}
-
-	int ans = 0;
-	for (int i = 0; i < M; i++) {
-		ans += team[i].score;
 	}
 	cout << ans;
 }
